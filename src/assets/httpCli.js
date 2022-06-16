e40d1c86634e17939317b38cd02940902a1c5e68
Juank1965 <juancarlosbastidasq@gmail.com>
2022-06-16 14:57:38 -0500

    Configurando variable de entorno

--- src/assets/httpCli.js
+++ src/assets/httpCli.js
@@ -1,11 +1,10 @@
 const API = "https://api.themoviedb.org/3";
+const AUTH_KEY = process.env.REACT_APP_AUTH_KEY;
 
 export function getData(path) {
   return fetch(API + path, {
     headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NmI2MzI3NWVhMWNmYzZiYTYyNDdlOTQ3NWUzYTY2YSIsInN1YiI6IjYyNTIwYzIxMDkxOTFiMDA2NTc2NjNjZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MresvjDFB87emJbxJIw8eqVniMmOfQABdEHBZ57eqJk",
-      "content-type": "application/json;charset=utf-8",
+      Authorization: `Bearer ${AUTH_KEY}`,
     },
   }).then((response) => response.json());
 }
